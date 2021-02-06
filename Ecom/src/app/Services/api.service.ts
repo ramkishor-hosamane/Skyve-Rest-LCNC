8c93e90e139da8793c5c9d3b8515ac4d2ff9dd4a
Ramkishor H <ramhosamane@gmail.com>
2021-02-06 13:55:23 +0530

    Fixed get request from angular to skyve

--- Ecom/src/app/Services/api.service.ts
+++ Ecom/src/app/Services/api.service.ts
@@ -9,11 +9,17 @@ export class ApiService {
   constructor(private http:HttpClient) { }
   baseURL = "http://localhost:8080/restLCNC/rest/json/product/Product";
   //baseURL = "http://localhost:3000/getProduct";
-
-  httpHeaders = new HttpHeaders({'Content-type':'application/json'});
-
+  authorizationData = 'Basic ' + btoa('setup' + ':' + 'setup');
+  
+  httpHeaders = new HttpHeaders({'Content-type':'application/json','Authorization': this.authorizationData});
+  headerOptions = {
+    headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': this.authorizationData
+    })
+};
   getAllProducts(): Observable<any>{
-    return this.http.get(this.baseURL,{headers:this.httpHeaders})
+    return this.http.get(this.baseURL,this.headerOptions)
   }
 
 }
--- Ecom/src/app/Services/submit-product.service.ts
+++ Ecom/src/app/Services/submit-product.service.ts
@@ -7,11 +7,22 @@ import { Product } from './../product';
 })
 export class SubmitProductService {
 
-  _url='http://localhost:3000/addProduct';
+  //_url='http://localhost:3000/addProduct';
+  _url = "http://localhost:8080/restLCNC/rest/json/insert/";
+
   constructor(private _http:HttpClient) { }
 
+  authorizationData = 'Basic ' + btoa('setup' + ':' + 'setup');
+  httpHeaders = new HttpHeaders({'Content-type':'application/json','Authorization': this.authorizationData});
+  data = {"bizModule":"product",
+          "bizDocument":"Product",
+          "name":"Biscuit","price":"30",
+          "bizId":"3c36be46-318d-4037-8a29-2438f093d35g",
+          "bizCustomer":"unisys",
+          "bizDataGroupId":null,
+          "bizUserId":"47d425e8-2e4c-4c31-b8de-b021171d3bb4"}
   submitData(product:Product){
-    return this._http.post<any>(this._url,product);
+    return this._http.post<any>(this._url,this.data,{headers:this.httpHeaders});
   }
 
 }
